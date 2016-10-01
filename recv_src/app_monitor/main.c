4332906e0e118a6a76b1f763c84f5c14010ef8b9
yahyazadeh <moosa-yahyazadeh@uiowa.edu>
2016-10-01 17:10:23 -0500

    Modification for phaser

--- recv_src/app_monitor/main.c
+++ recv_src/app_monitor/main.c
@@ -10,11 +10,12 @@
 #include "../phaser_msg.h"
 #include "../db_framework.h"
 
-// Comment below for less output
-// #define PRINT_PACKETS 1
-
 
 #define RATE_DELAY 200
+
+// Phaser configuration message
+MSG_NEW_WITH_ID(ant_msg, phaser_ping_t, PH_MSG_Test);
+phaser_ping_t *ant_cfg_p = &(ant_msg.payload);
 
 // Phaser control message(s)
 MSG_NEW_WITH_ID(ctrl_msg, phaser_control_t, PH_MSG_Control);
@@ -23,6 +24,35 @@ phaser_control_t *ctrl_data_p = &(ctrl_msg.payload);
 // Define a buffer for receiving messages
 MSG_DEFINE_BUFFER_WITH_ID(radioBuffer, recv_data_p, RADIO_MAX_PACKET);
 
+bool doneRecv=false;
+bool captData=false;
+data_record_t data_record_a[200];
+int recIdx = 0;
+int rxIdx=0;
+
+// -------------------------------------------------------------------------
+// Setup the phaser
+// -------------------------------------------------------------------------
+void phaser_init()
+{
+    // Init the test infrastructure
+    lastAngle = ANGLE_NOT_SET_VALUE;    // Force stepper angle recalibration
+    set_angle( -20 );
+    set_angle( 0 );
+    
+    // Init the iterators
+    testIdx.power.idx = 0;
+    testIdx.angle.idx = 0;
+    testIdx.angle.limit = test_config.angle_count;
+    
+    // Init the antena configuration
+    ant_cfg_p->expIdx = 0;
+    ant_cfg_p->msgCounter = 0;
+    ant_cfg_p->angle = 0;
+    ant_cfg_p->power = test_config.power[0];
+    
+    ant_test_init(&testIdx, &test_config, ant_cfg_p);
+}
 
 // --------------------------------------------
 
@@ -49,52 +79,8 @@ test_config_t test_config = {
 
 
 
-
-
-//===========================================
-//  Experimental data
-//===========================================
-#define NUM_EXPS 128
-
-static experiment_t experiment[NUM_EXPS];
-experiment_t * curExp = NULL;
-
-int lastExpIdx=0;
-
-int rxIdx=0;
-
-static bool flRestart=true;
-
-int phaseA;
-int phaseB;
-
 // Prototypes
 void send_ctrl_msg(msg_action_t act);
-
-
-// --------------------------------------------
-// Serial receive handler
-// --------------------------------------------
-#define SER_BUF_SIZE 64
-static uint8_t serBuffer[SER_BUF_SIZE];
-
-void onSerRecv(uint8_t bytes)
-{
-    // int i;
-    // PRINTF("Pong: ");
-    // for(i=0; i<bytes; i++)
-    // {
-    //     PRINTF("%c", (char)serBuffer[i]);
-    // }
-    // PRINTF("\n");
-
-    if(bytes>=1 && serBuffer[0] == 'r'){
-        PRINTF("Ser: Restart!\n");
-        flRestart = true;
-        send_ctrl_msg(MSG_ACT_RESTART);
-    }
-
-}
 
 
 // --------------------------------------------
@@ -106,136 +92,7 @@ void send_ctrl_msg(msg_action_t act)
     MSG_RADIO_SEND( ctrl_msg );
 }
 
-// --------------------------------------------
-// --------------------------------------------
-// void sendTestResults(int expIdxFrom, int expIdxTo)
-void sendTestResults()
-{
-    int rssi_mean, lqi_mean;
-    int32_t rssi_devSq, lqi_devSq;
-    experiment_t *exp = curExp;
 
-    curExp = NULL;  // Avoid retransmission of the same experiment
-
-    // int i;
-    // for (i=expIdxFrom; i<=expIdxTo; i++)
-    {
-        // exp = &(experiment[i]);
-        rssi_mean = STREAM_STAT_MEAN(exp->rssi_data);
-        rssi_devSq = STREAM_STAT_DEVIATION_SQUARED(exp->rssi_data);
-        lqi_mean = STREAM_STAT_MEAN(exp->lqi_data);
-        lqi_devSq = STREAM_STAT_DEVIATION_SQUARED(exp->lqi_data);
-        PRINTF("Test:"
-            "\t%d"
-            "\t%d\t%d\t%d"
-            // "\t%d\t%d\t%d\t%d\t%ld"
-            // "\t%d\t%d\t%d\t%d\t%ld"
-            "\t%d\t%d\t%d"
-            "\t%ld\t%ld"
-            "\t%d\t%d"
-            "\n",
-            (int) lastExpIdx,
-
-            (int) exp->power,
-            (int) exp->angle,
-            (int) exp->phase,
-
-            // (int) exp->rssi_data.sum,
-            // (int) exp->rssi_data.sum_squares,
-
-            // (int) exp->lqi_data.num,
-            // (int) exp->lqi_data.sum,
-            // (int) exp->lqi_data.sum_squares,
-
-            (int) exp->rssi_data.num,
-            (int) rssi_mean,
-            (int) lqi_mean,
-            (long unsigned int) (rssi_devSq),
-            (long unsigned int) (lqi_devSq),
-            (int) phaseA,
-            (int) phaseB
-            )
-        // debugHexdump((uint8_t *) exp, sizeof(experiment_t));
-
-        // Clear data
-        exp->power = 0;
-        exp->angle = 0;
-        exp->phase = 0;
-        STREAM_STAT_INIT(exp->rssi_data);
-        STREAM_STAT_INIT(exp->lqi_data);
-    }
-    //...
-
-}
-
-// --------------------------------------------
-// --------------------------------------------
-inline void processTestMsg(phaser_ping_t * test, rssi_t rssi, lqi_t lqi)
-{
-    // x
-    int expIdx;
-    // expIdx = test->expIdx;
-    // if( expIdx<0 || expIdx>= NUM_EXPS) return;
-
-    //Override caching data in array. use only one record.
-    expIdx =0;
-
-    experiment_t *exp = &(experiment[expIdx]);
-
-    // exp->expIdx = test->expIdx;
-
-    exp->power = test->power;
-    exp->angle = test->angle;
-    exp->phase = test->ant.phaseA | test->ant.phaseB ;
-    phaseA = test->ant.phaseA;
-    phaseB = test->ant.phaseB;
-    STREAM_STAT_ADD(exp->rssi_data, rssi);
-    STREAM_STAT_ADD(exp->lqi_data, lqi);
-
-    curExp = exp;
-    lastExpIdx = test->expIdx;
-}
-
-// --------------------------------------------
-// --------------------------------------------
-void printAction(action)
-{
-    PRINTF("Rx: %s\n", MSG_ACT_NAME( action ) );
-}
-
-// --------------------------------------------
-// --------------------------------------------
-void print_test_config(test_config_t *test_config)
-{
-    int pid = test_config->platform_id;
-    PRINTF("\nPlatform: %s\n", PH_PLATFORM_NAME(pid) );
-
-    PRINTF("Start_delay=%d\tSend_delay=%d\t Send_count=%d\n",
-        (int) test_config->start_delay,
-        (int) test_config->send_delay,
-        (int) test_config->send_count);
-
-    PRINTF("Angle_step=%d\tAngle_count=%d\n",
-        (int) test_config->angle_step,
-        (int) test_config->angle_count);
-
-    int pw, i=0;
-    PRINTF("TX_power:")
-    while( (pw=test_config->power[i++]) ){
-        PRINTF("\t%d", pw);
-    }
-    PRINTF("\n");
-
-    PRINTF("Ant_config:\t%d\t%d\t%d\t%d\t%d\t%d\n",
-        (int) test_config->ant.phaseA.start,
-        (int) test_config->ant.phaseA.step,
-        (int) test_config->ant.phaseA.count,
-        (int) test_config->ant.phaseB.start,
-        (int) test_config->ant.phaseB.step,
-        (int) test_config->ant.phaseB.count);
-
-    PRINTF("\n");
-}
 
 
 // --------------------------------------------
@@ -244,128 +101,110 @@ void onRadioRecv(void)
 {
     static bool flRxProcessing=false;
     if(flRxProcessing){
-#ifdef PRINT_PACKETS
-        PRINTF("RX Locked\n");
-#endif
         return;
     }
     flRxProcessing=true;    // There is a chance for a small race condition
-
-#ifdef PRINT_PACKETS
-    uint32_t rxTime = getTimeMs();
-#endif
-    int16_t rxLen;
-    rssi_t rssi;
-    lqi_t lqi;
-
-    rxIdx++;
-    if( rxIdx < 0 ) rxIdx=0;
-
-
-    led1Toggle();
-
-    // rxLen = radioRecv( &(DB_REC(db)), DB_REC_SIZE(db));
-    // DB_REC(db).recLen = rxLen;
-    rxLen = radioRecv(&radioBuffer, sizeof(radioBuffer));
-    rssi = radioGetLastRSSI();
-    lqi = radioGetLastLQI();
-
-#ifdef PRINT_PACKETS
-    PRINTF("%d\t%d\t%d\t%d\t%ld\t", (int)rxIdx, (int)rxLen, (int)rssi, (int)lqi, (long)rxTime);
-#endif
-#ifdef PRINT_PACKETS
-    if (rxLen < 0) {
-        PRINTF("RX failed\n");
-    }
-    else if (rxLen > 0 ) {
-        debugHexdump((uint8_t *) &radioBuffer, rxLen);
-        // debugHexdump((uint8_t *) &(DB_REC(db)), rxLen);
-    }
-#endif
-    if (rxLen < 0) {
-        led2Toggle();
-        flRxProcessing=false;
-        return;
-    }
-
-    if( ! MSG_SIGNATURE_OK(radioBuffer) ) { flRxProcessing = false; return; }
-
-    // Anticipated payload types.
-    MSG_NEW_PAYLOAD_PTR(radioBuffer, phaser_ping_t, test_data_p);
-    MSG_NEW_PAYLOAD_PTR(radioBuffer, phaser_control_t, ctrl_data_p);
-    MSG_NEW_PAYLOAD_PTR(radioBuffer, msg_text_data_t, msg_text_p);
-    MSG_NEW_PAYLOAD_PTR(radioBuffer, test_config_t, test_config_p);
-
-    int act = MSG_ACT_CLEAR;
     bool flOK=true;
-
-    switch( radioBuffer.id ){
-    case PH_MSG_Test:
+    
+    //Control payload.
+    MSG_NEW_PAYLOAD_PTR(radioBuffer, phaser_control_t, control_p);
+    MSG_NEW_PAYLOAD_PTR(radioBuffer, phaser_ping_t, test_data_p);
+    
+    if (radioBuffer.id == PH_MSG_Control) {
+        if (control_p->action == MSG_ACT_DONE) {
+            doneRecv = true;
+        }
+    }
+    
+    if (radioBuffer.id == PH_MSG_Test) {
         MSG_CHECK_FOR_PAYLOAD(radioBuffer, phaser_ping_t, flOK=false );
         if( !flOK ){
-            PRINTF("BadChk\n");
             break;
         }
-        // Check if new experiment iteration started.
-        if(lastExpIdx != test_data_p->expIdx && curExp){
-            sendTestResults();
-        }
-        processTestMsg(test_data_p, rssi, lqi);
-        break;
-
-    case PH_MSG_Angle:
-        if(curExp) sendTestResults();
-        if( flRestart ){        // Best time to resend the restart message after the angle change
-            send_ctrl_msg(MSG_ACT_RESTART);
-            flRestart = false;
-        }
-        break;
-
-    case PH_MSG_Control:
-        MSG_CHECK_FOR_PAYLOAD(radioBuffer, phaser_control_t, break);
-        if(curExp) sendTestResults();
-
-        act = ctrl_data_p->action;
-        if(act == MSG_ACT_START ){
-            flRestart = false;  // Clear restart command attempt
-        }
-        printAction(act);
-        break;
-
-    case PH_MSG_Text:
-        MSG_CHECK_FOR_PAYLOAD(radioBuffer, msg_text_data_t, break );
-        PRINTF(msg_text_p->text);
-        PRINTF("\n");
-        break;
-
-    case PH_MSG_Config:
-        MSG_CHECK_FOR_PAYLOAD(radioBuffer, test_config_t, break );
-        PRINTF("Config received:\n");
-        // TODO: parse the config and print
-        print_test_config(test_config_p);
+        captData=true;
     }
-
-
+    
+    
+    if (captData) {
+        
+        int16_t rxLen;
+        rssi_t rssi;
+        lqi_t lqi;
+        
+        rxIdx++;
+        if( rxIdx < 0 ) rxIdx=0;
+        
+        
+        led1Toggle();
+        
+        if( ! MSG_SIGNATURE_OK(radioBuffer) ) {
+            flRxProcessing = false;
+            return;
+        }
+        
+        rxLen = radioRecv(&radioBuffer, sizeof(radioBuffer));
+        
+        if (rxLen < 0) {
+            led2Toggle();
+            flRxProcessing=false;
+            return;
+        }
+        
+        rssi = radioGetLastRSSI();
+        lqi = radioGetLastLQI();
+        
+        data_record_a[recIdx]->rxIdx=rxIdx;
+        data_record_a[recIdx]->rssi=rssi;
+        data_record_a[recIdx]->lqi=lqi;
+        
+        recIdx++;
+        captData=false;
+    }
+    
     flRxProcessing=false;
+}
+
+// -------------------------------------------------------------------------
+// -------------------------------------------------------------------------
+void send_string(char *str)
+{
+    int len = strlen(str);
+    if(len>0 && len<MSG_TEXT_SIZE_MAX-1){
+        memcpy(text_msg.payload.text, str, len);
+        
+        radioSetTxPower(RADIO_MAX_TX_POWER);
+        MSG_RADIO_SEND( text_msg );
+    }
+}
+
+// -------------------------------------------------------------------------
+// -------------------------------------------------------------------------
+void sending_start()
+{
+    
+    for(i = 0; i < 199; i++)
+    {
+        send_string("%d\t%d\t%d\t", (int)data_record_a[i]->rxIdx, (int)data_record_a[i]->rssi, (int)data_record_a[i]->lqi);
+    }
+    
+    send_ctrl_msg(MSG_ACT_DONE);
 }
 
 // --------------------------------------------
 // --------------------------------------------
 void appMain(void)
 {
-    serialEnableRX(PRINTF_SERIAL_ID);
-    // serialSetReceiveHandle(PRINTF_SERIAL_ID, onSerRecv);
-    serialSetPacketReceiveHandle(PRINTF_SERIAL_ID, onSerRecv, serBuffer, SER_BUF_SIZE);
-
+    
     radioSetReceiveHandle(onRadioRecv);
     radioOn();
     mdelay(200);
-
-    // Send restart message to the phaser
-    send_ctrl_msg(MSG_ACT_RESTART);
-
+    
     while (1) {
-        mdelay(RATE_DELAY);
+        if (doneRecv) {
+            led1Toggle();
+            phaser_init();
+            sending_start();
+        }
         led0Toggle();
     }
 }
